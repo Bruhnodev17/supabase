bfe82e292f66e34cad720c2ea9e3e0233c1b5fcc
Manoel Bruno <manoelbrunop17@gmail.com>
2024-12-23 14:29:32 -0300

    Build App

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View,Text,StyleSheet} from "react-native"
+
+export default function Login(){
+    return(
+        <View>
+            <Text>
+                Página Login
+            </Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+
+})
