5a9c5c643d92240ea661fb9d7056b847c3f80b89
Manoel Bruno <manoelbrunop17@gmail.com>
2024-12-23 14:45:32 -0300

    resetting structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,11 @@ import { Stack } from "expo-router";
 
 export default function MainLayout(){
   return(
-    <Stack></Stack>
+    <Stack>
+      <Stack.Screen
+       name="index"
+       options={{ headerShown: false}}
+       />
+    </Stack>
   )
 }
