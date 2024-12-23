7d8c81094782dc90201a7ee6e7608cdc6962d7a0
Manoel Bruno <manoelbrunop17@gmail.com>
2024-12-23 17:01:30 -0300

    header of aplication

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,6 +3,6 @@ export default {
   zinc: "#0c0c1b",
   black: "#000",
   white: "#FFF",
-  green: "#99CFD1",
+  green: "#99CFD2",
   gray: "#DDD",
 };
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,43 @@
-import { View, Text, StyleSheet} from "react-native"
+import Colors from "@/constants/colors"
+import { View, Text, StyleSheet } from "react-native"
 
-export default function Login(){
-    return(
+export default function Login() {
+    return (
         <View style={styles.container}>
-            <Text>
-                Página Login
-            </Text>
+
+            <View style={styles.header}>
+                <Text style={styles.logoText}>Dev
+                    <Text style={{color: Colors.green}}>App</Text>
+                </Text>
+                <Text style={ styles.slogan}>
+                    Authenticator
+                </Text>
+            </View>
+
+            
+
         </View>
     )
 }
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        paddingTop: 40,
+        backgroundColor: Colors.zinc,
+    },
+    header:{
+        paddingLeft: 14,
+        paddingRight:14,
+    },
+    logoText:{
+        fontSize: 20,
+        fontWeight: "bold",
+        color:Colors.gray,
+        marginBottom: 8,
+    },
+    slogan:{
+        fontSize:34,
+        color: Colors.white,
+        marginBottom:34,
     }
 })
